7dd38e113ac81310032c59b4c51616121891f0e9
Philipp Legner <philipp@mathigon.org>
2015-11-14 01:32:37 +0000

    fix index.js

--- src/index.js
+++ src/index.js
@@ -5,20 +5,15 @@
 
 
 
-import Draw from 'draw';
-import Dropdown from 'dropdown';
-import Frame from 'frame';
-import Gallery from 'gallery';
-import Graph from 'graph';
-import Lightbox from 'lightbox';
-import Popup from 'popup';
-import Scrollreveal from 'scrollreveal';
-import Slider from 'slider';
-import SVG from 'svg';
-import Target from 'target';
-import Variable from 'variable';
-
-global.Slate = {
-    Draw, Dropdown, Frame, Gallery, Graph, Lightbox, Popup, Scrollreveal,
-    Slider, SVG, Target, Variable
-};
+export * from 'draggable';
+export * from 'drawing';
+export * from 'dropdown';
+export * from 'gallery';
+export * from 'graph';
+export * from 'imgSequence';
+export * from 'media';
+export * from 'parallax';
+export * from 'popup';
+export * from 'progress';
+export * from 'slider';
+export * from 'target';
