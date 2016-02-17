ffcef66af5b14cbfef967d55bfb3d881ac3fcb76
Philipp Legner <philipp@mathigon.org>
2016-02-16 21:14:10 -0800

    target + tooltip improvements

--- src/target/target.js
+++ src/target/target.js
@@ -17,78 +17,96 @@ const $targets = $N('svg', {
         width: 100%; height: 100%;
         display: none;
         pointer-events: none;
-        z-index: 900;`,
-    'html': `
-        <defs><mask id="masking">
+        z-index: 900;
+        opacity: 0;
+        transition: opacity .3s;`,
+    'html': `<defs><mask id="masking">
             <rect width="100%" height="100%" fill="white"/>
         </mask> </defs>
         <rect x="0" y="0" width="100%" height="100%" mask="url(#masking)" fill="white" opacity="0.9"/>`
 }, $body);
 
 const $mask = $targets.find('mask');
+let active = null;
+let $bounds = [];
 
 
 export default customElement('x-target', {
 
     created: function($el) {
-
-        let $bounds;
         let query = $el.attr('to');
+        let noMargins = $el.hasAttribute('no-margins');
 
         function enter() {
+            active = true;
 
-            let bounds = $$(query).map(x => x.bounds)
-                .filter(x => x.width && x.height);
+            let bounds = $$(query).map(x => x.bounds).filter(x => x.width && x.height);
             if (!bounds.length) return;
 
             let top = Math.min(...bounds.map(x => x.top));
             let bottom = Math.max(...bounds.map(x => x.top + x.height));
 
             let scrollUp = Browser.height - 20 - bottom;
-            let scrollDown = 20 - top;
+            let scrollDown = 20 + 40 - top;  // additional 40 for top navigation bar
 
             let scroll = scrollUp < 0 ? scrollUp : scrollDown > 0 ? scrollDown : 0;
             if (scroll) $body.scrollBy(-scroll, 300);
 
+            $bounds.forEach($b => { $b.remove(); });
             $bounds = [$el.bounds].concat(bounds).map(function(b, i) {
-                let margin = i ? 10 : 4;  // the target element itself gets no margin
+                let margin = (i && !noMargins) ? 10 : 4;  // the target element itself gets no margin
                 return $N('rect', {
-                    x: b.left - margin - scroll,
-                    y: b.top - margin - scroll,
+                    x: b.left - margin,
+                    y: b.top - margin + scroll,
                     width: b.width + 2 * margin,
                     height: b.height + 2 * margin,
                     rx: 4, ry: 4
                 }, $mask);
             });
 
+            $targets.css('display', 'block');
             Browser.redraw();
-            $targets.enter(300, 'fade', scroll ? 300 : 0);
+            setTimeout(function() { $targets.css('opacity', 1); }, scroll ? 300 : 0);
 
             /* TODO target arrows
              var targetBounds = $el.offset();
-             var dx = bounds[0].geo.left + bounds[0].geo.width/2  - targetBounds.left - 17;
-             var dy = bounds[0].geo.top  + bounds[0].geo.height/2 - targetBounds.top  - 17 - scroll;
+             var dx = bounds[0].left + bounds[0].width/2  - targetBounds.left - 17;
+             var dy = bounds[0].top  + bounds[0].height/2 - targetBounds.top  - 17 - scroll;
              var angle = 45 + Math.atan2(dy, dx) * 180 / Math.PI;
              $arrow.transform('rotate(' + Math.round(angle) + 'deg)');
              */
+
+            return scroll;
         }
 
         function exit() {
-            if (!$bounds) return;
-            let $oldBounds = $bounds;
+            if (!active) return;
+            active = false;
+            $targets.css('opacity', 0);
 
-            $el.transform = 'none';
-            $targets.fadeOut(300);
             setTimeout(function() {
-                $oldBounds.forEach(function($b) { $b.remove(); });
+                if (!active) $targets.css('display', 'none');
             }, 300);
         }
 
-        $el.on('mouseenter touchstart', enter);
-        $el.on('mouseleave touchend', exit);
+        $el.on('mouseenter', function() {
+            let didScroll = enter();
+            if (didScroll) {
+                setTimeout(function() {
+                    $body.one('scroll mousemove', exit);
+                }, 400);
+            } else {
+                $body.one('mousewheel', exit);
+                $el.one('mouseleave', exit);
+            }
+        });
+
+        $el.on('touchstart', function() {
+            enter();
+            $body.one('touchend touchmove', exit);
+        });
 
         $el.on('click', function() {
-            console.log($(query));
             $(query).trigger('click mousedown');
             exit();
         });
--- src/tooltip/tooltip.js
+++ src/tooltip/tooltip.js
@@ -5,41 +5,68 @@
 
 
 
-import { $, customElement, $body } from 'elements';
+import { $, $N, customElement, $body } from 'elements';
 
+// -----------------------------------------------------------------------------
+
+const $tooltip = $N('div', { class: 'tooltip'}, $body);
+let show = null;
+
+function hideTooltip() {
+    show = null;
+    $tooltip.removeClass('active');
+}
+
+function showTooltip($target, value, posn) {
+    $tooltip.text = value;
+    $tooltip.attr('class', 'tooltip ' + posn);
+
+    // TODO fit to window dimensions, maybe scroll?
+    let bounds = $target.bounds;
+
+    let x = (posn == 'left') ? bounds.left - $tooltip.width - 8 :
+        (posn == 'right') ? bounds.right + 8 :
+        bounds.left + bounds.width/2 - $tooltip.width/2;
+
+    let y = (posn == 'top') ? bounds.top - $tooltip.height - 8 :
+        (posn == 'bottom') ? bounds.bottom + 8 :
+        bounds.top + bounds.height/2 - $tooltip.height/2;
+
+    $tooltip.css({ left: x + 'px', top: y + 'px '});
+    $tooltip.addClass('active');
+}
+
+function makeTooltip($target, value, posn) {
+    $target.on('mouseover', function() {
+        if (show || $target.hasClass('active')) return;
+        show = $target;
+
+        setTimeout(function() {
+            if (show === $target) showTooltip($target, value, posn);
+        }, 300);
+    });
+
+    $target.on('mouseout', function() {
+        show = null;
+        setTimeout(function() { if (show != $target) hideTooltip(); }, 10);
+    });
+
+    $target.on('click', hideTooltip);
+}
+
+$body.on('scroll', hideTooltip);
+
+// -----------------------------------------------------------------------------
 
 export default customElement('x-tooltip', {
-
     created: function($el) {
 
-        $el.text = $el.attr('value');
-        $el.addClass($el.attr('posn') || 'top');
-
         let _for = $el.attr('for');
+        let posn = $el.attr('class') || 'top';
+
         let $target = _for ? $(_for) : $el.prev;
-        $el.moveTo($body);
+        if ($target) makeTooltip($target, $el.text, posn);
 
-        $target.on('mouseover', function() {
-            let bounds = $target.bounds;
-            let x = bounds.left + bounds.width/2 - $el.width/2;
-            let y = bounds.top - 30;
-
-            $el.css({ left: x + 'px', top: y + 'px '});
-            $el.addClass('active');
-        });
-
-        $target.on('mouseout', function() {
-            $el.removeClass('active');
-        });
-
-        $body.on('scroll', function() {
-            $el.removeClass('active');
-        });
-
-    },
-
-    detached: function($el) {
-        // TODO remove body event listener
+        $el.remove();
     }
-
 });
